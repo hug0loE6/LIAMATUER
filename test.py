0d7601972051f3b83b2155ba9cec025acb4b0406
hug0loE6 <hugo.lopez.20036@gmail.com>
2026-03-04 16:58:39 +0100

    test lecture ennonce d un exo

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ if len(sys.argv) != 2:
 
 token = sys.argv[1]
 driver = webdriver.Chrome()
+listeURLEexo = []
 
 driver.get("http://195.220.87.134/#activity=exercises")
 time.sleep(4)
@@ -25,10 +26,27 @@ except:
 try:
     time.sleep(4)
     listeexos = []
-    for l in driver.find_elements(By.CSS_SELECTOR, "#learnocaml-main-exercise-list ul li") : 
+    for l in driver.find_elements(By.CSS_SELECTOR, "#learnocaml-main-exercise-list > ul") : 
         listeexos.append(l)
-    
+    i = 0
+    for l in listeexos : 
+        print("section ", i)
+        i += 1
+        for url in l.find_elements(By.CSS_SELECTOR, "ul li a") :
+            listeURLEexo.append(url.get_attribute("href"))
+    for url in listeURLEexo :
+        print(url)
 except:
     print("zebi")
     driver.quit()
-
+#j'ai tout les url la
+test = listeURLEexo[0]
+try:
+    driver.get(test)
+    time.sleep(5)
+    iframe = driver.find_element(By.CSS_SELECTOR, "iframe")
+    driver.switch_to.frame(iframe)
+    buddy = driver.find_element(By.CSS_SELECTOR, "body")
+    print(buddy.text)
+finally:
+    driver.quit()
